f4e7b6021227e1a5daf6d887e4685af6a329a38e
StarshipzZ <1915349279@qq.com>
2022-03-13 15:20:58 +0800

    first commit from idea

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Hide on bush
+ * @date 2022/3/13 - 15:13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git from idea");
+    }
+}
